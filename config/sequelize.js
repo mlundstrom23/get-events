d4bd94b57acfd66268d36781071027ef0c83cc17
Marina <marina@Marinas-MacBook-Air.local>
2019-12-15 00:26:41 -0500

    All is working

---
+++ config/sequelize.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+module.exports = {
+    development: {
+        apikey: process.env.TICKETMASTER_API_KEY
+    }
+}
--- index.js
+++ index.js
@@ -1,51 +1,154 @@
-const Alexa = require('ask-sdk-core')
-const axios = require('axios')
+const Alexa = require('ask-sdk-core');
+const https = require('https')
 
 function getEvent(city, day) {
   const apikey = process.env.TICKETMASTER_API_KEY
-  var date1 = `${day}` + 'T14:00:00';
-  const url = `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&localStartDateTime=${date1},${date1}&apikey=${apikey}`
+  
+  const url = `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&localStartDateTime=${day}T00:00:00,${day}T23:59:59&apikey=${apikey}`
 
-  return axios.get(url).then(function (response) {
-    return response.data._embedded.events[0].name
-  })
-}
-
-const EventIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
-      handlerInput.requestEnvelope.request.intent.name === 'EventIntent'
-  },
-  handle(handlerInput) {
-    const city = handlerInput.requestEnvelope.request.intent.slots.city.value;
-    const day = handlerInput.requestEnvelope.request.intent.slots.day.value;
-
-    return getEvent(city, day).then(function (name) {
-      const speechText = `The events happening in ${city} on ${day} are <emphasis>${name}</emphasis>`
-
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .getReponse()
+    return new Promise((resolve) => {
+      https.get(url, (resp) => {
+        let data = '';
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          const event = JSON.parse(data)._embedded.events;
+            var event1 = Array.prototype.map.call(event, function(item) { return item.name }).join(", ");
+              event1 = Array.from(new Set(event1.split(','))).toString()
+                var event2 = event1.replace(/&/g, "and")
+                  event2 = event2.replace(/,([^,]*)$/, ', and$1')
+                    resolve(event2);
+        });
+      }).on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
     })
-  }
-}
+} 
 
-const ErrorHandler = {
-    canHandle() {
-        return true
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
+        const speakOutput = 'Welcome to Ticketmaster Events. When and where do you want to go out?';
         return handlerInput.responseBuilder
-            .speak("I'm sorry, I don't think there's any events happening that day. Would you like to search for a different date or city?")
-            .reprompt("Sorry, I didn't quite catch that. Can you please repeat that for me?")
-            .withShouldEndSession(false)
-            .getReponse()
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+const EventIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'EventIntent';
     },
-}
+    handle(handlerInput) {
+        const slots = handlerInput.requestEnvelope.request.intent.slots;
+        const city = slots['city'].value;
+        const day = slots['day'].value
+        
+        return getEvent(city, day).then((event2) => {
+        const speakOutput = `The events happening in ${city} on ${day} are <amazon:emotion name="excited" intensity="high">${event2}</amazon:emotion>`; 
+            return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .reprompt('Would you like to search for more events?')
+                .getResponse();
+        })  
+    }
+};
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say hello to me! How can I help?';
 
-const builder = Alexa.SkillBuilders.custom()
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
 
-exports.handler = builder
-  .addRequestHandlers(EventIntentHandler)
-  .addErrorHandlers(ErrorHandler)
-  .lambda()
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`~~~~ Error handled: ${error.stack}`);
+        const slots = handlerInput.requestEnvelope.request.intent.slots;
+        const city = slots['city'].value;
+        const day = slots['day'].value
+        const speakOutput = `Hmmm. Looks like there's no Ticketmaster events happening in ${city} on ${day}. Would you like to choose another time or city?`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        EventIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    )
+    .addErrorHandlers(
+        ErrorHandler,
+    )
+    .lambda();
